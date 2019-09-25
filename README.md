d2ddfe865e795fd2c69c8269c026ac749166d592
Andrew Weiss <wandrew8@gmail.com>
2019-09-24 20:34:52 -0700

    Liri bot fully functioning and readme updated

--- README.md
+++ README.md
@@ -1,35 +1,32 @@
 # Liri Node App
 
-
 ## Overview
 
-The LIRI Bot is a "Language Interpretation and Recognition Interface". It works by using set commands commands to retrieve information regarding movie and music information. 
+The LIRI Bot is a "Language Interpretation and Recognition Interface". It works by using set commands to retrieve information regarding movie and music information. This program uses many different third-party Node packages to retrieve the requested information. You can find a link to each of them here:
 
-Take a look at how some of the commands work:
+ [Axios](https://www.npmjs.com/package/axios)
+ [node-spotify-api](https://www.npmjs.com/package/node-spotify-api)
+ [moment](https://www.npmjs.com/package/moment)
+ [dotenv](https://www.npmjs.com/package/dotenv)
 
-1. The "concert-this" command
-![photo](images/concert-this.gif)
+Take a look at how the LIRI Bot works:
 
-2. The "movie-this" command
-![photo](images/movie-this.gif)
-
-3. The "spotify-this-song" command
-![photo](images/spotify-this-song.gif)
+![photo](images/liribot.gif)
 
 ## Installing 
 
 Follow the steps below to run the LIRI Bot in your own terminal:
 
-1. Within your terminal clone the git repository. You can find the repository [here](https://github.com/wandrew8/liri-node-app).
+1. Within your terminal clone [this](https://github.com/wandrew8/liri-node-app) git repository. 
 
-2. In order to run this program an API key from Spotify is required. You can sign up for an API Key [here](https://developer.spotify.com/dashboard/login)
+2. In order to run this program an API key from Spotify is required. You can sign up for an API Key [here](https://developer.spotify.com/dashboard/login).
 
 3. Once you've obtained your key, copy the information into a .env file with the format below. 
 
             SPOTIFY_ID=Your Spotify API Key 
             SPOTIFY_SECRET=Spotify Secret Number
 
- Note: Your API key and Spotify Secret should not contain spaces. Quotation marks are NOT needed. make sure the .env file is within the main directory of your cloned repository. 
+ Note: Your API key and Spotify Secret should not contain spaces or quotation marks. Make sure the .env file is within the main directory of your cloned repository. 
 
 4. Open the liri.js file in the terminal and install the packages using the following command. Press enter to install:
 
@@ -37,16 +34,26 @@ Follow the steps below to run the LIRI Bot in your own terminal:
 
 5. Now you are ready to use the LIRI Bot. Run the liri.js file in the terminal and type (node .\liri.js) followed by one of the commands below with the corresponding parameters.
 
-| Command | Parameter | Function |
-| ---------------- |: --------------------- :| -------------------------- :|
-| concert-this | Artist Name | Pulls a list of upcoming concerts |
-| spotify-this-song | Artist Name | Pulls up artist and album info |
-| movie-this | Movie Title | Pulls up information on the movie |
-| do-what-this-says | none | Pulls a command and parameter from an external text file |
+## How It Works
+
+Within the liri.js terminal enter the following into the command line:
+
+            node .\liri.js 
+            
+Follow the above with one of the commands below along with its required parameters. Then press ENTER to run the command.
+
+concert-this "Artist Name" | This command pulls a list of upcoming concerts for the specified artist.
+spotify-this-song "Artist Name" | This command pulls up artist and album info for the corresponding artist.
+movie-this "Movie Title" | This command pulls information on the searched movie.
+do-what-this-says "no parameters" | Pulls a command and parameter from an external text file. 
 
 Here are two examples of how to search using the LIRI Bot:
  - node .\liri.js concert-this taylor swift
  - node .\liri.js spotify-this-song like a rolling stone
+
+ ## Contact Me
+
+ I'm Andrew Weiss. If you have any questions of suggestions you can contact me at my github account [wandrew8](https://github.com/wandrew8)
 
 
 
--- liri.js
+++ liri.js
@@ -11,53 +11,59 @@ var fs = require("fs");
 var userCommand = process.argv[2];
 var userSearch = process.argv.slice(3).join(" ");
 
-if (userCommand === "concert-this") {
-    concert();
-} else if (userCommand === "movie-this") {
-    movie();
-} else if (userCommand === "spotify-this-song") {
-    spotifySong();
-} else if (userCommand === "do-what-it-says") {
-    userSearch = 
-    doIt();
-}   else {
-    console.log("That is an invalid command. Try one of these commands instead: concert-this, movie-this, spotify-this-song, or do-what-it-says")
+function appCommands(userCommand, userSearch) {
+
+    if (userCommand === "concert-this") {
+        concert();
+    } else if (userCommand === "movie-this") {
+        movie();
+    } else if (userCommand === "spotify-this-song") {
+        spotifySong();
+    } else if (userCommand === "do-what-it-says") {
+        doIt(userSearch);
+    } else {
+        console.log("That is an invalid command. Try one of these commands instead: concert-this, movie-this, spotify-this-song, or do-what-it-says")
+    }
 }
 
+appCommands(userCommand, userSearch);
 
 function concert() {
 
     var queryUrl = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp";
-    
-    axios
-    .get(queryUrl)
-    .then(function(response) {
-        console.log("Here are the concert details for " + userSearch + "!")
-        
-        for (let i=0; i<response.data.length; i++) {
-            var venueName = response.data[i].venue.name;
-            var venueLocation = response.data[i].venue.country;
-            var date = moment(response.data[i].datetime).format("MM-DD-YYYY");
-            
 
-            console.log(`- ${venueName} ${venueLocation} ${date}`);
-        }
-    })
-    .catch(function(error) {
-        throw error;          
-    });
+    axios
+        .get(queryUrl)
+        .then(function (response) {
+            console.log("Here are the concert details for " + userSearch + "!\n")
+
+            for (let i = 0; i < response.data.length; i++) {
+                var venueName = response.data[i].venue.name;
+                var venueLocation = response.data[i].venue.country;
+                var date = moment(response.data[i].datetime).format("MM-DD-YYYY");
+
+                console.log("-------------------------------------")
+                console.log(venueName);
+                console.log(venueLocation);
+                console.log(date);
+                console.log("-------------------------------------\n")
+            }
+        })
+        .catch(function (error) {
+            throw error;
+        });
 }
 
 function movie() {
+    if (!userSearch) {userSearch = "mr nobody";};
+    
     var queryUrl = "http://www.omdbapi.com/?t=" + userSearch + "&y=&plot=short&apikey=trilogy";
 
     axios
-    .get(queryUrl)
-    .then(function(response) {
-        if (userSearch === "") {
-            userSearch = "mr nobody"
-        } 
-            console.log("Here are some details about the movie you chose!")
+        .get(queryUrl)
+        .then(function (response) {
+            console.log("-------------------------------------")
+            console.log("Here are some details about the movie you chose!\n")
             console.log("Title: " + response.data.Title);
             console.log("Year of Release: " + response.data.Year);
             console.log("IMDB Score: " + response.data.imdbRating);
@@ -66,51 +72,40 @@ function movie() {
             console.log("Language: " + response.data.Language);
             console.log("Synopsis: " + response.data.Plot);
             console.log("Actors: " + response.data.Actors);
-        
-    });
+            console.log("-------------------------------------\n")
+        });
 
 }
-
 function spotifySong() {
-      
-      spotify.search({ type: 'track', query: userSearch, limit: 1}, function(err, data) {
+
+    spotify.search({ type: 'track', query: userSearch, limit: 1 }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
-        console.log("Here are details for the song " + userSearch + "!")
-      console.log("Artist: " + data.tracks.items[0].album.artists[0].name); 
-      console.log("Song Title: " + userSearch);
-      console.log("Link to Song: " + data.tracks.items[0].album.external_urls.spotify);
-      console.log("Album: " + data.tracks.items[0].album.name); 
-      });
+        console.log("-------------------------------------")
+        console.log("Here are details for the song " + userSearch + "!\n")
+        console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+        console.log("Song Title: " + userSearch);
+        console.log("Link to Song: " + data.tracks.items[0].album.external_urls.spotify);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("-------------------------------------\n")
+
+    });
 }
 
 function doIt() {
-    fs.readFile("random.txt", "utf8", function(err, data) {
 
-        if (err) {
-          console.log(err);
-        }
-      
-        else {
-            var dataArr = data.split(",");
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {console.log(error);}
+        
+        var dataArr = data.split(",");
 
-            if (dataArr[0] === "concert-this") {
-                console.log("This is working")
-                // concert();
-            } else if (dataArr[0] === "movie-this") {
-                console.log("This is working")
-                // movie();
-            } else if (dataArr[0] === "spotify-this-song") {
-                console.log("This is working")
-                // spotifySong();
-            } else if (dataArr[0] === "do-what-it-says") {
-                console.log("This is working")
-                // doIt();
-            }   else {
-                console.log("Hmm...let's add another command to the list")
-            }
-      
-        }
-    })
-}
+        userCommand = dataArr[0];
+        userSearch = dataArr[1];
+
+        appCommands(userCommand, userSearch);
+    });
+};
+
+
+
